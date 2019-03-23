74a46e95260526b2994cb6eb04955b8546d0a3dc
Ryan Gross <rgross@osisoft.com>
2019-03-23 11:20:21 -0400

    initial commit of source

---
+++ Grades/GradeBook.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    public class GradeBook
+    {
+        public GradeBook()
+        {
+            grades = new List<float>();
+        }
+
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            var stats = new GradeStatistics();
+            var sum = 0f;
+
+            foreach (var grade in grades)
+            {
+                sum += grade;
+
+                stats.HighestGrade = Math.Max(stats.HighestGrade, grade);
+                stats.LowestGrade = Math.Min(stats.LowestGrade, grade);
+            }
+
+            stats.AverageGrade = sum / grades.Count;
+
+
+            return stats;
+        }
+
+        List<float> grades;
+    }
+}
---
+++ Grades/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var speech = new SpeechSynthesizer();
+            speech.Speak("Hello! This is the grade book program.");
+
+            var book = new GradeBook();
+            book.AddGrade(91f);
+            book.AddGrade(89.5f);
+            book.AddGrade(75f);
+
+
+            var gradeStats = book.ComputeStatistics();
+            Console.WriteLine($"The highest grade in the book is {gradeStats.HighestGrade}");
+            Console.WriteLine($"The lowest grade in the book is {gradeStats.LowestGrade}"); 
+            Console.WriteLine($"All grades averaged {gradeStats.AverageGrade}");
+
+        }
+    }
+}
